f107d27648cee2c3f7e30ddd294fa6262996fcc7
Ryan Stillings <rystills@gmail.com>
2018-10-19 13:59:15 -0400

    gitignore + template main file, include nuklear + license for GUI

---
+++ src/MIPS-sandbox.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : MIPS-sandbox.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello, world!");
+	return EXIT_SUCCESS;
+}
